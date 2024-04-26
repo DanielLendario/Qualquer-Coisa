6570518e289c57b304e363c2e89da4fc0c0cf6e7
DanielLendario <161842026+DanielLendario@users.noreply.github.com>
2024-04-26 09:09:58 -0300

    Update Contador e calculadora.py

--- Contador e calculadora.py
+++ Contador e calculadora.py
@@ -1,6 +1,21 @@
-contador = 1                                                                          
+contador = 1
 while contador < 100000:
   contador = + 1
   print("contador: ", contador)
   if contador >= 100000:
     print("Você chegou a 100000 pontos")
+
+Calculadora
+n1 = float(input("digite o primeiro número: "))
+n2 = float(input("digte o segundo número: "))
+operacao = input("digite a operação(+-*/): ")
+if operacao == "+" :
+  resultado = n1 + n2
+elif operacao == "-" :
+    resultado = n1 - n2
+elif operacao == "*" :
+    resultado n1 * n2
+elif operacao == "/" :
+  resultado = n1 / n2
+else print ("Erro Tente Novamente")
+print(resultado)
