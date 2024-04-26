627a6b2d0deb673e5b5b29bdcd20e84ab527e13c
DanielLendario <161842026+DanielLendario@users.noreply.github.com>
2024-04-26 08:48:44 -0300

    Create Contador e calculadora.py

---
+++ Contador e calculadora.py
@@ -0,0 +1,6 @@
+contador = 1                                                                          
+while contador < 100000:
+  contador = + 1
+  print("contador: ", contador)
+  if contador >= 100000:
+    print("Você chegou a 100000 pontos")
